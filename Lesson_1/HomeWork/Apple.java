2188c52d6fa8c330a883c603d8d5d25d3aeea8d2
GuitaristNSK <GuitaristNSK@yandex.ru>
2018-08-14 16:32:43 +0700

    Lesson_2_HomeWork

--- Lesson_1/HomeWork/Apple.java
+++
@@ -1,7 +0,0 @@
-package Lesson_1.HomeWork;
-
-public class Apple extends Fruit {
-    public Apple() {
-        super(1.0f);
-    }
-}
--- Lesson_1/HomeWork/Orange.java
+++
@@ -1,7 +0,0 @@
-package Lesson_1.HomeWork;
-
-public class Orange extends Fruit {
-    public Orange() {
-        super(1.5f);
-    }
-}
